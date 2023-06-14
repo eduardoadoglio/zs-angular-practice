cfd981331132a7028119cf2116c7ab5fa85695e2
Eduardo Adoglio <eduardoadoglio4@gmail.com>
2023-06-14 08:47:38 -0300

    Gerenciamento correto das credenciais do usuário

--- src/shared/auth/services/auth/auth.service.ts
+++ src/shared/auth/services/auth/auth.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {Observable, tap} from "rxjs";
+import {map, Observable, tap} from "rxjs";
 import {Router} from "@angular/router";
 import {User} from "../../../models/user";
 import {SignUpModel} from "../../../models/signup";
+import {Credentials} from "../../../models/credentials";
 
 @Injectable({
   providedIn: 'root'
@@ -15,14 +16,31 @@ export class AuthService {
   constructor(
     private httpClient: HttpClient,
     private router: Router,
-  ) { }
+  ) {
+  }
 
-  login(username: string, password: string): Observable<User> {
-    return this.httpClient.post<User>(`${this.apiUrl}login/`, {username, password});
+  login(username: string, password: string): Observable<Credentials> {
+    return this.httpClient.post(`${this.apiUrl}login/`, {username, password})
+      .pipe(
+        map((response: any) => {
+          return new Credentials({
+            accessToken: response?.access,
+            refreshToken: response?.refresh
+          })
+        }),
+        tap((credentials) => this.setSession(credentials))
+      );
   }
 
   signUp(data: SignUpModel): Observable<User> {
     return this.httpClient.post<User>(`${this.apiUrl}signup/`, {...data})
+  }
+
+  setSession(credentials: Credentials): void {
+    localStorage.setItem('accessToken', credentials.accessToken);
+    localStorage.setItem('refreshToken', credentials.refreshToken);
+    localStorage.setItem('accessTokenExpiration', JSON.stringify(credentials.accessTokenExpiration));
+    localStorage.setItem('refreshTokenExpiration', JSON.stringify(credentials.refreshTokenExpiration));
   }
 
 
