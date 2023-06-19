cde1fcfcb0dbf11e47885f0658dd5463becb0d70
Eduardo Adoglio <eduardoadoglio4@gmail.com>
2023-06-19 15:52:59 -0300

    Implementação inicial do módulo de home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {path: '', redirectTo: '/auth/login', pathMatch: 'full'},
-  {path: 'auth', loadChildren: () => import('../shared/auth/auth.module').then(m => m.AuthModule)}
+  {path: 'auth', loadChildren: () => import('../shared/auth/auth.module').then(m => m.AuthModule)},
+  {path: 'home', loadChildren: () => import('../shared/home/home.module').then(m => m.HomeModule)}
 ];
 
 @NgModule({
