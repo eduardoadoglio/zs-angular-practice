c448d6fa6290a0c1929981227c4cad6c725d569c
Eduardo Adoglio <eduardoadoglio4@gmail.com>
2023-06-23 10:52:34 -0300

    Aumento temporário no tempo de expiração dos tokens

--- src/shared/models/credentials.ts
+++ src/shared/models/credentials.ts
@@ -3,10 +3,10 @@ export class Credentials {
   refreshToken: string = '';
 
   accessTokenExpiration: Date;
-  private readonly ACCESS_TOKEN_EXPIRATION_IN_SECONDS = 270;
+  private readonly ACCESS_TOKEN_EXPIRATION_IN_SECONDS = 86400;
 
   refreshTokenExpiration: Date;
-  private readonly REFRESH_TOKEN_EXPIRATION_IN_SECONDS = 86400;
+  private readonly REFRESH_TOKEN_EXPIRATION_IN_SECONDS = 1296000;
   public constructor(init?: Partial<Credentials>) {
     Object.assign(this, init);
     const now = new Date();
@@ -17,8 +17,8 @@ export class Credentials {
   fromLocalStorage(): Credentials {
     this.accessToken = localStorage.getItem('accessToken') ?? '';
     this.refreshToken = localStorage.getItem('refreshToken') ?? '';
-    this.accessTokenExpiration = new Date(Date.parse(localStorage.getItem('accessTokenExpiration') ?? ''));
-    this.refreshTokenExpiration = new Date(Date.parse(localStorage.getItem('refreshTokenExpiration') ?? ''));
+    this.accessTokenExpiration = new Date(Date.parse(JSON.parse(localStorage.getItem('accessTokenExpiration') ?? '')));
+    this.refreshTokenExpiration = new Date(Date.parse(JSON.parse(localStorage.getItem('refreshTokenExpiration') ?? '')));
     return this;
   }
 }
